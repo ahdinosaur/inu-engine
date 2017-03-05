b9a11847c7a77212760aba8fdafec8b58d666c07
Michael Williams <michael.williams@enspiral.com>
2017-03-06 11:49:12 +1300

    some ideas

--- index.js
+++ index.js
@@ -1,5 +1,43 @@
-var start = require('./start')
-module.exports = start
-module.exports.defaults = require('./defaults')
-module.exports.default = start
-module.exports.start = start
+const { keys } = Object
+const Store = require('inu')
+const Watch = require('mutant/watch')
+
+
+var watch = Store(function (state, action, { emit, watch }) {
+  console.log('state is', state) // state while action is being emitted
+  console.log('action is', action) // action being emitted
+  console.log('emit is:', emit) // function to emit actions
+  console.log('watch is:', watch) // object of functions to watch inu observables
+})
+
+function Store (update = identity) {
+
+  const state = Value()
+  const action = Value()
+  const view = computed(([state, action]) => {
+    nextState = update(state, action, store)
+    if (nextState !== undefined) state.set(nextState)
+  })
+
+  const obs = { state, view, action }
+  const watch = keys(obs).reduce((sofar, key) => {
+    sofar[key] = Watcher(obs[key])
+    return sofar
+  }, {})
+
+  const store = { emit, watch }
+
+  return store
+
+  function emit (action) {
+    action.set(action)
+  }
+}
+
+function Watcher (obs) {
+  return listener => Watch(obs, listener)
+}
+
+function identity (value) {
+  return value
+}
